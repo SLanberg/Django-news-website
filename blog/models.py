41996ee8405bb709ce20c9c10bce65f1222a2e67
Stephan <stefan.lanberg@gmail.com>
2020-03-12 05:25:16 +0200

    Models Category - added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+class Category(models.Model):
+
+
+	# Category model
+	name = models.CharField(verbose_name='Name', max_length=100)
+	slug = models.SlugField(verbose_name='url', max_length=100)
+
+
+
+	def __str__(self):
+		return self.name
+
+
+
+	class Meta:
+		verbose_name_plural = 'categories'
